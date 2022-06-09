9002ed134d6a9c3a43e24d9235281264d0ef92d6
Macarena Peche <macarena.peche@toptal.com>
2022-06-09 18:25:40 +0200

    Add RSpec test for Day_3 Part II

--- Day_3/binary_diagnostic.rb
+++ Day_3/binary_diagnostic.rb
@@ -6,7 +6,7 @@ class BinaryDiagnostic
     @list = list
   end
 
-  def rates
+  def gamma_epsilon_rates
     n = list[0].length
     gamma = ""
     epsilon = ""
@@ -16,11 +16,49 @@ class BinaryDiagnostic
       gamma = ones > zeroes ? gamma + "1" : gamma + "0"
       epsilon = ones < zeroes ? epsilon + "1" : epsilon + "0"
     end
-    [gamma, epsilon] 
+    [gamma.to_i(2), epsilon.to_i(2)] 
+  end
+
+  def o2_rate
+    n = list[0].length
+    o2 = list.map(&:clone)
+
+    (0...n).each do |i|
+      return o2.map { |item| item.to_i(2) } if o2.length == 1
+      ones = o2.select { |item| item[i] == "1" }.count 
+      zeroes = o2.select { |item| item[i] == "0" }.count
+      chosen = ones >= zeroes ? "1" : "0"
+      o2.select! { |item| item[i] == chosen }
+    end
+    o2.map { |item| item.to_i(2) }
+  end
+
+  def co2_rate
+    n = list[0].length
+    co2 = list.map(&:clone)
+
+    (0...n).each do |i|
+      return co2.map { |item| item.to_i(2) } if co2.length == 1
+      ones = co2.select { |item| item[i] == "1" }.count 
+      zeroes = co2.select { |item| item[i] == "0" }.count
+      chosen = ones >= zeroes ? "0" : "1"
+      co2.select! { |item| item[i] == chosen }
+    end
+    co2.map { |item| item.to_i(2) }
   end
 
   def power_consumption 
-    gamma_epsilon = rates
-    gamma_epsilon[0].to_i(2) * gamma_epsilon[1].to_i(2)
+    gamma_epsilon = gamma_epsilon_rates
+    gamma_epsilon[0] * gamma_epsilon[1]
+  end
+
+  def life_support
+    o2_rate[0] * co2_rate[0]
   end
 end
+
+
+arr = File.readlines('input.txt', chomp: true) 
+binary_diagnostic = BinaryDiagnostic.new(arr)
+puts binary_diagnostic.power_consumption
+puts binary_diagnostic.life_support
--- Day_3/binary_diagnostic_spec.rb
+++ Day_3/binary_diagnostic_spec.rb
@@ -3,16 +3,34 @@ require './binary_diagnostic.rb'
 RSpec.describe BinaryDiagnostic do 
   subject(:diagnostic) { BinaryDiagnostic.new(["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"])}
   
-  it 'has a rates method' do
-    rates = diagnostic.rates 
+  it 'has a gamma_epsilon_rates method' do
+    rates = diagnostic.gamma_epsilon_rates 
 
-    expect(rates).to eq(["10110", "01001"])
+    expect(rates).to eq([22, 9])
+  end
+
+  it 'has a o2_rate method' do
+    rate = diagnostic.o2_rate
+
+    expect(rate).to eq([23])
+  end
+
+  it 'has a co2_rate method' do
+    rate = diagnostic.co2_rate
+
+    expect(rate).to eq([10])
   end
 
   it 'has a power consumption method' do
     power_consumption = diagnostic.power_consumption 
 
     expect(power_consumption).to eq(198) 
+  end
+
+  it 'has a life_support_rating' do
+    life_support = diagnostic.life_support
+
+    expect(life_support).to eq(230)
   end
 
   context 'when given an invalid input' do
